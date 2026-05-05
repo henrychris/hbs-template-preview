347f994ea0119e675e36de3064c9ca3912ccb64f
Henry Ihenacho <64085210+henrychris@users.noreply.github.com>
2026-05-05 23:44:06 +0100

    add posthog reverse proxy (#2)

--- src/hooks.client.ts
+++ src/hooks.client.ts
@@ -1,13 +1,21 @@
 import posthog from 'posthog-js';
 import type { HandleClientError } from '@sveltejs/kit';
-import { PUBLIC_POSTHOG_HOST, PUBLIC_POSTHOG_PROJECT_TOKEN } from '$env/static/public';
+import {
+	PUBLIC_POSTHOG_HOST,
+	PUBLIC_POSTHOG_PROJECT_TOKEN,
+	PUBLIC_POSTHOG_UI_HOST
+} from '$env/static/public';
+import { dev } from '$app/environment';
 
 export async function init() {
-	posthog.init(PUBLIC_POSTHOG_PROJECT_TOKEN, {
-		api_host: PUBLIC_POSTHOG_HOST,
-		defaults: '2026-01-30',
-		capture_exceptions: true
-	});
+	if (!dev) {
+		posthog.init(PUBLIC_POSTHOG_PROJECT_TOKEN, {
+			api_host: PUBLIC_POSTHOG_HOST,
+			ui_host: PUBLIC_POSTHOG_UI_HOST,
+			defaults: '2026-01-30',
+			capture_exceptions: true
+		});
+	}
 }
 
 export const handleError: HandleClientError = async ({ error, status, message }) => {
